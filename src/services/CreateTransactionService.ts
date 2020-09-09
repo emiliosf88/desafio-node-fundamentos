de0c41f24a122115365079bb94a5dc0afd20f701
Emilio Fernandes <emilio.fernandes-extern@renault.com>
2020-09-09 17:26:05 -0300

    Desafio Node Transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+// Service = Regra de negócios
+
+interface Request{
+  title: string,
+  value: number,
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute( {title, value, type}:Request ): Transaction {
+
+    if (!["income", "outcome"].includes(type)){
+      throw new Error("Invalid transaction");
+    }
+
+    const { total }= this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value){
+      throw new Error ( "Not enough balance");
+    }
+    // tem balance? getbalance tem que retornar o valor e checar se o não vai ficar negativo
+
+    const transaction = this.transactionsRepository.create({
+      title, 
+      value, 
+      type,
+    });
+    
+    return transaction;
   }
 }
 
